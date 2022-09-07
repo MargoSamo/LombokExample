d46d0f6ee7ef143ca064ca88106e5d564f70bcfd
MargoSamo <margo_ii@mail.ru>
2022-09-07 16:05:18 +0400

    dependencies lombok

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(new Wife(3, "Maria").getName());
+    }
+}
---
+++ src/main/java/org/example/Wife.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor()
+public class Wife {
+    private int age;
+    private String name;
+}
